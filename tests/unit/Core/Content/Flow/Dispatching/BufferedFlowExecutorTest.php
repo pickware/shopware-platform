effd157ba96c72b214ee64a2384ff2a01379e450
Benedikt Brunner <benedikt.brunner@pickware.de>
2025-01-20 14:18:48 +0100

    Fix lint in tests
    Signed-off-by: Benedikt Brunner <benedikt.brunner@pickware.de>

--- tests/unit/Core/Content/Flow/Dispatching/BufferedFlowExecutorTest.php
+++ tests/unit/Core/Content/Flow/Dispatching/BufferedFlowExecutorTest.php
@@ -20,7 +20,6 @@ use Shopware\Core\Content\Flow\Dispatching\StorableFlow;
 use Shopware\Core\Content\Flow\Dispatching\Struct\Flow;
 use Shopware\Core\Content\Flow\Exception\ExecuteSequenceException;
 use Shopware\Core\Content\Flow\FlowException;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Uuid\Uuid;
